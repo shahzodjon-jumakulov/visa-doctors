cc04831d6508af2c9250a7acd5f3b70f81c4d314
shahzodjon-jumakulov <shahzod3137@gmail.com>
2025-03-08 00:07:11 +0500

    hide captcha badge

--- plugins/vue-recaptcha-v3.client.ts
+++ plugins/vue-recaptcha-v3.client.ts
@@ -5,5 +5,8 @@ import { VueReCaptcha } from "vue-recaptcha-v3";
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueReCaptcha as any, {
     siteKey: "6LdD5eYqAAAAAKDva7Yf5V7kjzWtpEHJftZPloWc",
+    loaderOptions: {
+      autoHideBadge: true,
+    },
   });
 });
